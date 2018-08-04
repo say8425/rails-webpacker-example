e6658f0db7a6be8ff0d0168f240ed4caa0916b42
Penguin <say8425@gmail.com>
2018-08-04 22:08:24 +0900

    webpacker 설정에 jquery와 popper 를 provide plugin으로 추가, 이게 없으면 에러 발생
    https://stackoverflow.com/questions/45998003/how-to-use-jquery-with-rails-webpacker-3/46211463#46211463

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,18 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    JQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+}));
+
+const config = environment.toWebpackConfig()
+
+config.resolve.alias = {
+    jquery: "jquery/src/jquery",
+}  
+
 module.exports = environment
